d63bed803d4cb60f98363735efc90323f0716720
Mark Probst <mark.probst@gmail.com>
2011-06-17 11:50:22 +0200

    Swank support.

--- compiler/project.clj
+++ compiler/project.clj
@@ -1,3 +1,4 @@
 (defproject compiler "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]])
