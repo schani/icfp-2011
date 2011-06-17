fa5a8a7a727fb7c4b4fba2f74c94329465ca8327
Heinz Deinhart <heinz@nix.at>
2011-06-17 23:12:43 +0200

    added basic argument parsing, alt mode works

--- interpreter/tester.ml
+++ interpreter/tester.ml
@@ -1,8 +1,37 @@
 open Inter
 open Parser
 open Printer
+open Arg
+
+type running_mode =
+  | RM_UNKNOWN
+  | RM_ONLY
+  | RM_ALT
+  | RM_MATCH of (string * string)
+
+let runmode = ref RM_UNKNOWN
 
 let default_world_printer world = ()
 
 let _ =
-  play_game default_context (create_default_world ()) (parse_input stdin) (parse_input stdin) std_world_printer
+  let parse_otherargs str =
+    runmode :=
+      match (Str.split (Str.regexp " +") str) with
+	| ["only"] -> RM_ONLY
+	| ["alt"] -> RM_ALT
+	| ["match"; p1; p2] -> RM_MATCH (p1, p2)
+	| _ -> failwith "illegal argument"
+  in
+    Arg.parse [] parse_otherargs "only | alt | match <prog1> <prog2>";
+    match !runmode with
+      | RM_UNKNOWN -> failwith "dunno what to do, gimme some args"
+      | RM_ONLY -> failwith "not yet implemented"
+      | RM_ALT ->
+	  play_game default_context (create_default_world ())
+	    (parse_input stdin) (parse_input stdin) std_world_printer
+      | RM_MATCH (p1, p2) ->
+	  let p1_in_channel, p1_out_channel = Unix.open_process ("p1" ^ " 0")
+	  and p2_in_channel, p2_out_channel = Unix.open_process ("p1" ^ " 1")
+	  in
+	    play_game default_context (create_default_world ())
+	      (parse_input p1_in_channel) (parse_input p2_in_channel) std_world_printer
