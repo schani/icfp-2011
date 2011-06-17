95a4d933043efc05ce879116f77ad54e1cdf5dab
Mark Probst <mark.probst@gmail.com>
2011-06-17 12:07:30 +0200

    Simple lambda calculus to SKI compiler.

--- compiler/project.clj
+++ compiler/project.clj
@@ -1,4 +1,5 @@
 (defproject compiler "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [matchure "0.10.1"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
--- compiler/src/fikdb/compiler/core.clj
+++ compiler/src/fikdb/compiler/core.clj
@@ -1,1 +1,29 @@
-(ns fikdm.compiler.core)
+(ns fikdm.compiler.core
+  (:use matchure))
+
+(defn compile-a [x expr]
+  (cond-match expr
+
+	      [?M ?N]
+	      (list (list 'S (compile-a x M)) (compile-a x N))
+
+	      ?y
+	      (if (= x y)
+		'I
+		(list 'K y))))
+
+(defn compile-lambda [expr]
+  (cond-match expr
+
+	      [?lambda [?x] ?M]
+	      (if (= lambda 'fn)
+		(compile-a x (compile-lambda M))
+		(throw (Exception. (str "Not a valid expression: " expr))))
+
+	      [?M ?N]
+	      (list (compile-lambda M) (compile-lambda N))
+
+	      ?x
+	      x))
+
+;;(compile-lambda '(fn [x] (fn [y] x)))
