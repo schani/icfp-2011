912fee44aa01dac1ed1b52af884629161e90e5aa
Mark Probst <mark.probst@gmail.com>
2011-06-18 19:17:17 +0200

    Combinator functions for side effects.

--- compiler/src/fikdm/compiler/programs.clj
+++ compiler/src/fikdm/compiler/programs.clj
@@ -4,24 +4,9 @@
 
 (defvar *SII* (compile-lambda '(:fn [x] (x x))))
 
-(defn make-loop [side-effect]
-  `(((:S :I) :I)
-    (:fn [f#]
-	 ((:fn [y#]
-	    (((:S :I) :I) f#))
-	  ~side-effect))))
+;;; combinators
 
-(defn make-se-combine-fn [a b]
-  `((:S ~a) ~b))
-
-(defn make-apply-self-return [side-effect-fn]
-  (let [f (gensym 'f)]
-    `(~*SII*
-      (:fn [~f]
-	   ~(make-se-combine-fn side-effect-fn
-				`(:fn [d2#]
-				      (~f ~f)))))))
-
+;; A
 (defn make-se-fn [side-effect]
   (match-lambda side-effect
 		:lambda ([x M]
@@ -31,10 +16,46 @@
 		:primitive (x
 			    (throw (Exception. (str "Side effect cannot be a primitive: " side-effect))))))
 
+;; C
+(defn make-se-combine-fn [a b]
+  `((:S ~a) ~b))
+
+;; P
+(defn make-se-pass-fn [g e]
+  `(:fn [d#]
+	(~e (~g d#))))
+
+(defn make-loop [side-effect]
+  `(((:S :I) :I)
+    (:fn [f#]
+	 ((:fn [y#]
+	    (((:S :I) :I) f#))
+	  ~side-effect))))
+
+(defn make-apply-self-return [side-effect-fn]
+  (let [f (gensym 'f)]
+    `(~*SII*
+      (:fn [~f]
+	   ~(make-se-combine-fn side-effect-fn
+				`(:fn [d2#]
+				      (~f ~f)))))))
+
 (defn make-help-attack-fn [help-field help-strength attack-field attack-strength]
   (let [attack-fn (make-se-fn `(((:attack ~help-field) ~attack-field) ~attack-strength))
 	help-fn (make-se-fn `(((:help ~help-field) ~help-field) ~help-strength))]
     (make-se-combine-fn help-fn attack-fn)))
+
+;; takes the attack field as an argument
+(defn make-param-attack-fn [help-field attack-strength]
+  (let [attack-field (gensym 'attack-field)]
+    `(:fn [~attack-field]
+	  (((:attack ~help-field) ~attack-field) ~attack-strength))))
+
+;; takes the attack field from a slot
+(defn make-help-get-attack-fn [help-field help-strength attack-field-slot attack-strength]
+  (make-se-combine-fn (make-se-fn `(((:help ~help-field) ~help-field) ~help-strength))
+		      (make-se-pass-fn (make-se-fn `(:get ~attack-field-slot))
+				       (make-param-attack-fn help-field attack-strength))))
 
 (defn make-repeat-effect-fn [n side-effect-fn]
   (assert (>= n 2))
@@ -59,17 +80,37 @@
   (let [help-attack (make-help-attack 0 8192 0 768)]
     (make-apply-self-return (make-repeat-effect n help-attack))))
 
+(defn lambda->ski [program]
+  (optimize-ski (compile-lambda (pre-optimize-lambda program))))
+
+(defvar *regs* #{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27})
+
 (defn spit-echoer [filename program]
   (shell-script filename
 		(generate
-		 (optimize-ski (compile-lambda (pre-optimize-lambda program)))
-		 1 #{2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20})))
+		 (lambda->ski program)
+		 1 *regs*)))
+
+(shell-script "/tmp/beidler.sh"
+	      (concat
+	       (generate 0 64 nil)
+	       (generate (lambda->ski (list (make-help-get-attack-fn 0 8192 64 768) :I)) 65 *regs*)))
+
+(spit-echoer "/tmp/beidler.sh"
+	     (make-apply-self-return
+	      (make-se-combine-fn
+	      (let [slot (gensym 'slot)]
+		`((:fn [~slot]
+		       ~(make-repeat-effect-fn 15 (make-help-attack-fn 0 8192 slot 768)))
+		  ~(make-se-fn '(:get 64)))))))
 
 ;;(spit-echoer "/tmp/beidler.sh" (make-help-attack-loop 0 8192 0 768))
 ;;(spit-echoer "/tmp/beidler.sh" (make-dec-loop 0))
 ;;(spit-echoer "/tmp/beidler.sh" (make-repeat-effect 3 '(:dec :zero)))
-;;(spit-echoer "/tmp/beidler.sh" (list (make-repeat-effect-fn 15 (make-help-attack-fn 0 8192 0 768)) :I))
+;;(spit-echoer "/tmp/beidler.sh" (list (make-repeat-effect-fn 15 (make-help-attack-fn 0 8192 0 768)) :I)
 
 ;;(spit-echoer "/tmp/beidler.sh" (make-apply-self-return (make-help-attack-fn 0 8192 0 768)))
 
 ;;(spit-echoer "/tmp/beidler.sh" (make-help-attack-self-return 10 0 8192 0 768))
+
+;;(lambda->ski (make-apply-self-return (make-repeat-effect-fn 15 (make-help-attack-fn 0 8192 0 768))))
