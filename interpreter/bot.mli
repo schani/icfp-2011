1c8081b708b3a6c99b65abf3d3e48cef153322a4
Heinz Deinhart <heinz@nix.at>
2011-06-18 23:39:32 +0200

    enhanced loaderbot

--- interpreter/bot.mli
+++
@@ -1,3 +0,0 @@
-open Cards
-
-val botloop: ?skipfirst:bool -> (default_world_type intercontext -> default_world_type -> turn option -> a' -> turn * a') -> a' -> unit
--- interpreter/loaderbot.ml
+++ interpreter/loaderbot.ml
@@ -5,7 +5,10 @@ open Arg
 open Cards
 open Bot
 
-type privdata = { pd_turns : turn list }
+type privdata = {
+  pd_turns : turn list;
+  pd_stage : int
+}
 
 let read_turns_from_file ifi printer =
   let l = ref []
@@ -20,12 +23,19 @@ let read_turns_from_file ifi printer =
 	End_of_file -> List.rev !l
 
 let move_callback context world proponent_move privdata =
-  let move, rest =
-    match privdata.pd_turns with
-      | [] -> Left (I, 18), []
-      | turn :: rest -> turn, rest
+  let move, rest, stage =
+    match privdata.pd_stage with
+      | 0 (* kill enemy field 0, not yet implemented *)
+      | 1 -> (* apply commands from file *)
+	  begin
+	    match privdata.pd_turns with
+	      | turn :: [] -> turn, [], 2
+	      | turn :: rest -> turn, rest, 1
+	      | _ -> failwith "should not happen"
+	  end
+      | _ -> Left (I, 0), [], 2
   in
-    move, { pd_turns = rest }
+    move, { pd_turns = rest; pd_stage = stage }
 
 let _ =
   let ifi = open_in "/icfpnfs/SCHANI/killer-fn.cmd"
@@ -34,6 +44,6 @@ let _ =
     | [| _; "1" |] -> true
     | _ -> failwith "dunno what to do, gimme some args"
   in let turns = read_turns_from_file ifi quiet_printer
-  in let pd = { pd_turns = turns }
+  in let pd = { pd_turns = turns; pd_stage = 0 }
   in
     bootloop ~skipfirst move_callback pd
