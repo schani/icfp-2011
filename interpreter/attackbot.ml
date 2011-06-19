e8b43393e3362df78dbec144694e3db95ff2079c
Heinz Deinhart <heinz@nix.at>
2011-06-19 20:04:54 +0200

    made debug msgs bertl compliant

--- interpreter/attackbot.ml
+++ interpreter/attackbot.ml
@@ -5,6 +5,7 @@ open Arg
 open Cards
 open Bot
 open Cofu
+open Printf
 
 type state =
   | S_FIND_VICTIM of int
@@ -16,6 +17,9 @@ type privdata = {
   pd_state: state
 }
 
+let botdebug str =
+  output_string stderr ("% " ^ str);
+  flush stderr
 
 let calculate_64_er_slot_value slot_to_attack =
   255 - slot_to_attack
@@ -25,20 +29,20 @@ let move_callback context world proponent_move turn_stats privdata =
     let rec state_machine state =
       match state with
 	| S_FIND_VICTIM limit ->
-	    Printf.fprintf stderr "move_callback: S_FIND_VICTIM: limit=%i\n" limit;
+	    botdebug (sprintf "move_callback: S_FIND_VICTIM: limit=%i\n" limit);
 	    let victim = (last_alive_other_slot_custom world limit)
 	    in let job = write_number_to_slot (calculate_64_er_slot_value victim) 64
 	    in
 	      state_machine (S_APPLY_TURNS (job, S_MASR_LAUNCH_ATTACK victim))
 	| S_APPLY_TURNS (turns, stored_state) -> begin
-	    Printf.fprintf stderr "move_callback: S_APPLY_TURNS: %i left\n" (List.length turns);
+	    botdebug (sprintf "move_callback: S_APPLY_TURNS: %i left\n" (List.length turns));
 	    match turns with
 	      | turn :: [] -> turn, stored_state
 	      | turn :: rest -> turn, S_APPLY_TURNS (rest, stored_state)
 	      | [] -> state_machine stored_state
 	  end
 	| S_MASR_LAUNCH_ATTACK victim ->
-	    Printf.fprintf stderr "move_callback: S_MASR_LAUNCH_ATTACK: attacking %i\n" victim;
+	    botdebug (sprintf "move_callback: S_MASR_LAUNCH_ATTACK: attacking %i\n" victim);
 	    let vicvit, _ = context.read_other_vit victim world
 	    in
 	      if vicvit > 0 then
@@ -46,7 +50,7 @@ let move_callback context world proponent_move turn_stats privdata =
 	      else
 		state_machine (S_MASR_FIND_NEXT_VICTIM victim)
 	| S_MASR_FIND_NEXT_VICTIM old_victim ->
-	    Printf.fprintf stderr "move_callback: S_MASR_FIND_NEXT_VICTIM: old_victim=%i\n" old_victim;
+	    botdebug (sprintf "move_callback: S_MASR_FIND_NEXT_VICTIM: old_victim=%i\n" old_victim);
 	    let next_victim = last_alive_other_slot world
 	    in let possible_job = write_number_to_slot (calculate_64_er_slot_value next_victim) 64
 	    in
@@ -62,7 +66,7 @@ let move_callback context world proponent_move turn_stats privdata =
 	move, { pd_state = next_state }
   with
       Bot_error str ->
-	Printf.fprintf stderr "move_callback: Bot_error: %s\n" str;
+	botdebug ("move_callback: Bot_error: %s\n" ^ str);
 	Left (I, 0), privdata
 
 let _ =
