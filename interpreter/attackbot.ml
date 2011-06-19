4e58fc40c5c2287d4f1a0e7e0d41406cc190bf98
Heinz Deinhart <heinz@nix.at>
2011-06-19 23:54:02 +0200

    attackbot now resurrects (maybe)

--- interpreter/attackbot.ml
+++ interpreter/attackbot.ml
@@ -12,19 +12,34 @@ type state =
   | S_MASR_FIND_VICTIM of (default_world_type -> int)
   | S_MASR_LAUNCH_ATTACK of int
   | S_MASR_FIND_NEXT_VICTIM of int
+  | S_RESURRECT of (int list * state)
 
 type privdata = {
   pd_state: state;
   during_load_slots: int list;
   post_load_slots: int list;
 }
+    
+let calculate_64_er_slot_value slot_to_attack =
+  255 - slot_to_attack
+
+let rec find_dead_slots ?(result=[]) slots = function
+  | [] -> result
+  | slotnr :: rest ->
+      if (fst slots.(0)) <= 0 then
+	find_dead_slots ~result:(slotnr :: result) slots rest
+      else
+	find_dead_slots ~result slots rest
+
+let generate_ressurect_code world deadnr =
+  let alive_slot = last_alive_slotnr (fst world) 255
+  in let numgen = write_number_to_slot deadnr alive_slot
+  in
+    List.append numgen [(Left (Revive, alive_slot))]
 
 let botdebug str =
   output_string stderr ("% " ^ str);
   flush stderr
-
-let calculate_64_er_slot_value slot_to_attack =
-  255 - slot_to_attack
 
 let turns_masr = read_turns_from_file "functions/masr-65-64-init.cmd"
 let turns_masr4 = read_turns_from_file "functions/masr4.cmd"
@@ -33,21 +48,36 @@ let move_callback context world proponent_move turn_stats privdata =
   try
     let rec state_machine state privdata =
       match state with
+	| S_RESURRECT (deads, stored_state) -> begin
+	    match deads with
+	      | [] -> state_machine stored_state privdata
+	      | x :: rest ->
+		  let job = generate_ressurect_code world x
+		  in
+		    state_machine (S_APPLY_TURNS (job, S_RESURRECT (rest, stored_state))) privdata
+	  end
 	| S_APPLY_TURNS (turns, stored_state) -> begin
 	    botdebug (sprintf "move_callback: S_APPLY_TURNS: %i left\n" (List.length turns));
-	    match turns with
-	      | turn :: [] -> turn, stored_state, privdata
-	      | turn :: rest -> turn, S_APPLY_TURNS (rest, stored_state), privdata
-	      | [] -> state_machine stored_state privdata
+	    let deads = find_dead_slots (fst world) privdata.during_load_slots
+	    in
+	      if (List.length deads) > 0 then
+		(* oh no, we have been interrupted *)
+		state_machine (S_RESURRECT (deads, S_APPLY_TURNS (turns_masr, S_MASR_FIND_VICTIM biggest_other_slot)))
+		  privdata
+	      else
+		match turns with
+		  | turn :: [] -> turn, stored_state, privdata
+		  | turn :: rest -> turn, S_APPLY_TURNS (rest, stored_state), privdata
+		  | [] -> state_machine stored_state privdata
 	  end
 	| S_MASR_FIND_VICTIM selector ->
-(*	    botdebug ("VORM DEPPATEN SCHASS\n");*)
+	    (* botdebug ("VORM DEPPATEN SCHASS\n");*)
 	    let victim = selector world
 	    in
 	      botdebug (sprintf "move_callback: S_MASR_FIND_VICTIM: %i\n" victim);
 	      let job = write_number_to_slot (calculate_64_er_slot_value victim) 64
 	      in
-		state_machine (S_APPLY_TURNS (job, S_MASR_LAUNCH_ATTACK victim)) privdata
+		state_machine (S_APPLY_TURNS (job, (S_MASR_LAUNCH_ATTACK victim))) privdata
 	| S_MASR_LAUNCH_ATTACK victim ->
 	    botdebug (sprintf "move_callback: S_MASR_LAUNCH_ATTACK: attacking %i\n" victim);
 	    (
